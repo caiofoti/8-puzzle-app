6b0750ada793445e254338494f8db969f8633d5d
Caio Foti Pontes <caio.pontes@ufcspa.edu.br>
2024-10-01 22:49:08 -0300

    Refactor puzzle component imports, add HttpClientModule, and update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PuzzleComponent } from './puzzle/puzzle.component';
@@ -14,7 +15,9 @@ import { PuzzleComponent } from './puzzle/puzzle.component';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withFetch())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
